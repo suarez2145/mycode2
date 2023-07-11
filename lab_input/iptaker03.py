e13710328a1932af52cf606751323f48a487a5a9
suarez2145 <suarez.exxon@gmail.com>
2023-07-11 20:03:30 +0000

    mixed lists

---
+++ lab_input/iptaker03.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python3
+
+def main():
+
+    user_name_input = input("what is your name sir?")
+    user_day_input = input("what day of the week is it sir?")
+    print("Hello",user_name_input,"Happy",user_day_input,"!!!")
+main()
+
